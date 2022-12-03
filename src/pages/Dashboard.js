d376bac7840379c5ebba279be981380793fb5839
James Redding <jdr161@case.edu>
2022-12-03 17:02:24 -0500

    Pulls in user info from aws-amplify

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import logo from '../logo.svg';
 import '../App.css';
 import { Navigate  } from "react-router-dom";
 import { Component } from 'react';
+import { Auth } from 'aws-amplify';
 
 class Dashboard extends Component {
     constructor() {
@@ -9,6 +10,15 @@ class Dashboard extends Component {
         this.state = {
             navToHomepageBool: false
         };
+        Auth.currentAuthenticatedUser({
+            bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+        }).then(user => {
+            console.log(user);
+            this.setState({
+                username: user.attributes.preferred_username
+            })
+            })
+        .catch(err => console.log(err));
     }
     handleSignoutClick = () => {
         this.setState({navToHomepageBool: true});
@@ -21,7 +31,7 @@ class Dashboard extends Component {
                 <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Hello
+                    Hello {this.state.username}
                     <button onClick={this.handleSignoutClick}>Sign out</button>
                     This is the Dashboard
                 </p>
