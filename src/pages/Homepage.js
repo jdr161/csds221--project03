95ba1aea42310e07d7a5170fc6e430d8277d1fdf
James Redding <jdr161@case.edu>
2022-12-04 02:30:59 -0500

    removed old react styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,3 @@
-import logo from '../logo.svg';
-import '../App.css';
 import { Navigate  } from "react-router-dom";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -8,21 +6,16 @@ import '@aws-amplify/ui-react/styles.css';
 
 const Homepage = () => {
   return (
-    <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            This is the Homepage
-        </p>
-        <Authenticator>
+    <>
+      
+      <Authenticator>
           {() => (
             <main>
               <Navigate to="/dashboard" />
             </main>
           )}
         </Authenticator>
-        </header>
-    </div>
+    </>
   )
 };
 
